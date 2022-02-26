42f5d9aeedabd5ad96b7eebc30f6f398f466fc5b
Josep Alsina <josepalsinasio@gmail.com>
2022-02-26 15:11:17 +0100

    Updates

--- src/utils/utils.js
+++
@@ -1,6 +0,0 @@
-import { DateTime } from 'luxon';
-
-export function dateToRelativeDate(date) {
-    const myMoment = DateTime.fromISO(date);
-    return myMoment.setLocale('ca').toRelativeCalendar()
-}
