5877465d3352446af202b6305b76d701e198fe3b
Josep Alsina <josepalsinasio@gmail.com>
2022-03-06 17:04:24 +0100

    minor change

--- src/components/api_wrapper.js
+++ src/components/api_wrapper.js
@@ -1,12 +1,12 @@
 export const baseURL = 'https://zi9bgvb5e3.execute-api.eu-west-3.amazonaws.com/Prod/';
 
-const { REACT_APP_API_AUTHORIZATION } = process.env;
+// const { REACT_APP_API_AUTHORIZATION } = process.env;
 
 const pk = "BOT#BMR";
 const url = `${baseURL}${encodeURIComponent(pk)}`;
 
 export const getBotNames = async () => {
-    console.log(`REACT_APP_API_AUTHORIZATION = ${REACT_APP_API_AUTHORIZATION}`);
+    // console.log(`REACT_APP_API_AUTHORIZATION = ${REACT_APP_API_AUTHORIZATION}`);
 
     const resp = await fetch(url, {
         method: 'GET',
