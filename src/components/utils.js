9bfb486d367105cb3b7fb4779f9b7232baa691e4
Josep Alsina <josepalsinasio@gmail.com>
2022-04-03 18:15:54 +0200

    updated user-event

--- src/components/utils.js
+++ src/components/utils.js
@@ -1,5 +1,7 @@
 import { useEffect } from 'react';
 
+import process from "process";
+
 export function useTitle(title) {
     useEffect(() => {
         const prevTitle = document.title
@@ -8,4 +10,8 @@ export function useTitle(title) {
             document.title = prevTitle
         }
     })
+}
+
+export function isDev() {
+    return !process.env.NODE_ENV || process.env.NODE_ENV === 'development';
 }
