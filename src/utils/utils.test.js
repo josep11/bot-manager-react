7f55243e61d59db733c21d0484b92222ccbc37c4
Josep Alsina <josepalsinasio@gmail.com>
2022-07-28 10:55:09 +0200

    fixed tests

--- src/utils/utils.test.js
+++ src/utils/utils.test.js
@@ -1,16 +1,20 @@
 import { DateTime } from 'luxon';
 
-import { dateToRelativeDate } from "./utils";
+import { dateToRelativeDate, parseDateTime } from "./utils";
+
+
+
+test('parseDateTime', () => {
+    let dtStr = "2022-04-29T08:44:43";
+    const r = parseDateTime(dtStr);
+    expect(r).not.toBeFalsy();
+    expect(r).toBeInstanceOf(DateTime);
+});
 
 test('dateToRelativeDate', () => {
-    let dtStr = "2022-04-29T08:44:43";
     let dt = DateTime.now()
 
-    let r
-    r = dateToRelativeDate(dtStr)
-    expect(r).not.toBeFalsy();
-
-    r = dateToRelativeDate(dt)
+    const r = dateToRelativeDate(dt)
     expect(r).not.toBeFalsy();
     expect(r).toBe("avui");
 });
