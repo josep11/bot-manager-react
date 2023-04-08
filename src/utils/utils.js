d1ca3e6000e97e8fd5c1db9e4321e3069856713c
Josep Alsina <josep.alsina@addevent.com>
2023-04-08 11:32:45 +0200

    format utils

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,28 +1,32 @@
-import { DateTime } from 'luxon';
+import { DateTime } from "luxon";
 
 /**
- * 
- * @param {DateTime} date 
+ *
+ * @param {DateTime} date
  * @returns {?string} relative date expressed like "ahir"
  */
 export function dateToRelativeDate(date) {
-    if (!date) { return null }
-    if (!(date instanceof DateTime)) {
-        throw new Error("parameter is not of DateTime type");
-    }
-    if (!date.isValid) {
-        throw new Error(`date "${date}" is not a valid DateTime`);
-    }
-    return date.setLocale('ca').toRelativeCalendar()
+	if (!date) {
+		return null;
+	}
+	if (!(date instanceof DateTime)) {
+		throw new Error("parameter is not of DateTime type");
+	}
+	if (!date.isValid) {
+		throw new Error(`date "${date}" is not a valid DateTime`);
+	}
+	return date.setLocale("ca").toRelativeCalendar();
 }
 
 export function parseDateTime(date) {
-    if (typeof date !== "string") {
-        throw Error('Input parameter date is not string. It is of type: ' + (typeof date));
-    }
-    return DateTime.fromISO(date);
+	if (typeof date !== "string") {
+		throw Error(
+			"Input parameter date is not string. It is of type: " + typeof date
+		);
+	}
+	return DateTime.fromISO(date);
 }
 
 export function isDev() {
-    return !process.env.NODE_ENV || process.env.NODE_ENV === 'development';
+	return !process.env.NODE_ENV || process.env.NODE_ENV === "development";
 }
