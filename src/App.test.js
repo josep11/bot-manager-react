176df347d7989d463db07f28758ce8eb721911ee
Josep Alsina <josepalsinasio@gmail.com>
2025-05-08 00:09:31 +0200

    .

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,5 @@
 import { act, render, screen } from '@testing-library/react';
+import React from 'react';
 import App from './App';
 import { getBotNames } from './components/apiWrapper';
 import ListTable from './components/list';
