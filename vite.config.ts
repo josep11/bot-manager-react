7bfabd00e79a7d37d6ac388ae12f34dbaa85c41e
Josep Alsina <josepalsinasio@gmail.com>
2025-05-08 10:33:53 +0200

    use vite instead of old lib

---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [react()],
+    base: '/bot-manager-react/',
+    test: {
+        globals: true,
+        environment: 'jsdom',
+        setupFiles: './src/setupTests.ts',
+    },
+}); 
